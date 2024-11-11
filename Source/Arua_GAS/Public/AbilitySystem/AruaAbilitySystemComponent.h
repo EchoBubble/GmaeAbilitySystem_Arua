64d1d47b3b328deccae5922a6b2d7dc8dc3deb57
EchoBubble <1756865212@qq.com>
2024-11-11 11:21:43 +0800

    AbilitySystem Component And AttributeSet

---
+++ Source/Arua_GAS/Public/AbilitySystem/AruaAbilitySystemComponent.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AbilitySystemComponent.h"
+#include "AruaAbilitySystemComponent.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARUA_GAS_API UAruaAbilitySystemComponent : public UAbilitySystemComponent
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/Arua_GAS/Public/AbilitySystem/AruaAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "AruaAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ARUA_GAS_API UAruaAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
