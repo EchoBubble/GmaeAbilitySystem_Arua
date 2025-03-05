a73e0b9f595e63f3e1e25771bf282f678c12b087
EchoBubble <1756865212@qq.com>
2025-03-05 17:59:29 +0800

    Get All Asset Tags

--- Source/Arua_GAS/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
+++ Source/Arua_GAS/Private/AbilitySystem/AuraAbilitySystemComponent.cpp
@@ -11,5 +11,15 @@ void UAuraAbilitySystemComponent::AbilityActorInfoSet()
 void UAuraAbilitySystemComponent::EffectApplied(UAbilitySystemComponent* AbilitySystemComponent,
                                                 const FGameplayEffectSpec& EffectSpec, FActiveGameplayEffectHandle ActiveGameplayEffectHandle)
 {
-	GEngine->AddOnScreenDebugMessage(1,8.f,FColor::Blue,FString("Effect Applied!"));
+	//GEngine->AddOnScreenDebugMessage(1,8.f,FColor::Blue,FString("Effect Applied!"));
+	FGameplayTagContainer TagContainer;
+	EffectSpec.GetAllAssetTags(TagContainer);
+	for (const FGameplayTag& tag : TagContainer)
+	{
+		//TODO: Broadcast the tag to the Widget Controller
+		const FString Msg = FString::Printf(TEXT("GE Tag: %s"),*tag.ToString());
+		GEngine->AddOnScreenDebugMessage(-1, 5.0,FColor::Blue,Msg);
+		
+	}
+	
 }
