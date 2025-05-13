fc353696b56a21ea00d8793cb95e1c42a858c97d
EchoBubble <1756865212@qq.com>
2025-05-13 16:06:27 +0800

    Send Mouse Cursor Data

--- Source/Arua_GAS/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
+++ Source/Arua_GAS/Private/AbilitySystem/AbilityTasks/TargetDataUnderMouse.cpp
@@ -3,6 +3,8 @@
 
 #include "AbilitySystem/AbilityTasks/TargetDataUnderMouse.h"
 
+#include "AbilitySystemComponent.h"
+
 UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGameplayAbility* OwningAbility)
 {
 	UTargetDataUnderMouse* MyObj = NewAbilityTask<UTargetDataUnderMouse>(OwningAbility);
@@ -11,8 +13,44 @@ UTargetDataUnderMouse* UTargetDataUnderMouse::CreateTargetDataUnderMouse(UGamepl
 
 void UTargetDataUnderMouse::Activate()
 {
+	const bool bIsLocallyControlled = Ability->GetCurrentActorInfo()->IsLocallyControlled();
+	if (bIsLocallyControlled)//判断当前机器是否控制着这个角色，只有客户端 + listen server才会为true
+	{
+		SendMouseCursorData();
+	}
+	else
+	{
+		//TODO: We are on the server,so listen for target data.
+	}
+	
+	
+}
+
+void UTargetDataUnderMouse::SendMouseCursorData()
+{
+	//打开预测窗口 ➜ 生成新的 PredictionKey ➜ 存到 ASC->ScopedPredictionKey,默认asc的预测键为空
+	FScopedPredictionWindow ScopedPrediction(AbilitySystemComponent.Get());
+	
 	APlayerController* PC = Ability->GetCurrentActorInfo()->PlayerController.Get();
 	FHitResult CursorResult;
 	PC->GetHitResultUnderCursor(ECC_Visibility,false,CursorResult);
-	ValidData.Broadcast(CursorResult.Location);
+
+	FGameplayAbilityTargetDataHandle DataHandle;
+	FGameplayAbilityTargetData_SingleTargetHit* Data = new FGameplayAbilityTargetData_SingleTargetHit();//打包命中结果
+	Data->HitResult = CursorResult;
+	DataHandle.Add(Data);//传入给句柄
+
+	AbilitySystemComponent->ServerSetReplicatedTargetData(
+		GetAbilitySpecHandle(),
+		GetActivationPredictionKey(),
+		DataHandle,FGameplayTag(),
+		AbilitySystemComponent->ScopedPredictionKey);
+	//判断是否应该广播，因为有可能这个技能已经取消，打断，结束了，会导致无效，此时继续广播可能会导致选空指针
+	if (ShouldBroadcastAbilityTaskDelegates())
+	{
+		ValidData.Broadcast(DataHandle);
+	}
+
+
+
 }
