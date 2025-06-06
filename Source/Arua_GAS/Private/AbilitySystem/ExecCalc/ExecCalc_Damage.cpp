f0bc3209a10353b206075ebe6ced409ab52c7211
EchoBubble <1756865212@qq.com>
2025-06-06 16:39:42 +0800

    Damage Execution Calculation

---
+++ Source/Arua_GAS/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
+
+#include "AbilitySystemComponent.h"
+
+UExecCalc_Damage::UExecCalc_Damage()
+{
+	
+}
+
+void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
+	FGameplayEffectCustomExecutionOutput& OutExecutionOutput) const
+{
+	//获取源和目标的ASC组件，EC只能在GE应用到目标时才能获取TargetASC
+	const UAbilitySystemComponent* SourceASC = ExecutionParams.GetSourceAbilitySystemComponent();
+	const UAbilitySystemComponent* TargetASC = ExecutionParams.GetTargetAbilitySystemComponent();
+
+	//获取源和目标的角色实例，也就是场景中实际存在的角色
+	const AActor* SourceActor = SourceASC ? SourceASC->GetAvatarActor() : nullptr;
+	const AActor* TargetActor = TargetASC ? TargetASC->GetAvatarActor() : nullptr;
+
+	//获取当前GE的实例
+	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
+	
+}
