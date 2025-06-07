be0a37c3e14b86d725ea9482b99ce3bf293e2835
EchoBubble <1756865212@qq.com>
2025-06-07 12:00:35 +0800

    ExecCalcs-Capturing Attributes

--- Source/Arua_GAS/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
+++ Source/Arua_GAS/Private/AbilitySystem/ExecCalc/ExecCalc_Damage.cpp
@@ -4,10 +4,30 @@
 #include "AbilitySystem/ExecCalc/ExecCalc_Damage.h"
 
 #include "AbilitySystemComponent.h"
+#include "AbilitySystem/AuraAttributeSet.h"
+
+struct AuraDamageStatics
+{
+	//声明一个捕获定义，名字得和AS中的属性名一致
+	DECLARE_ATTRIBUTE_CAPTUREDEF(Armor);
+	
+	AuraDamageStatics()
+	{
+		//这个宏会根据传入的设置找到目标身上的属性，写构造是因为要实时运行
+		DEFINE_ATTRIBUTE_CAPTUREDEF(UAuraAttributeSet,Armor, Target, false)
+	}
+};
+
+static const AuraDamageStatics& DamageStatics()
+{
+	static AuraDamageStatics DStatics;
+	return DStatics;//返回全局唯一实例
+}
 
 UExecCalc_Damage::UExecCalc_Damage()
 {
-	
+	//将捕获的定义连带设置床欸数组，不然GAS读不到数据
+	RelevantAttributesToCapture.Add(DamageStatics().ArmorDef);
 }
 
 void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecutionParameters& ExecutionParams,
@@ -23,5 +43,20 @@ void UExecCalc_Damage::Execute_Implementation(const FGameplayEffectCustomExecuti
 
 	//获取当前GE的实例
 	const FGameplayEffectSpec& Spec = ExecutionParams.GetOwningSpec();
-	
+
+	//通过GE实例获取源和目标的角色以及GE上的所有标签Tag
+	const FGameplayTagContainer* SourceTags = Spec.CapturedSourceTags.GetAggregatedTags();
+	const FGameplayTagContainer* TargetTags = Spec.CapturedTargetTags.GetAggregatedTags();
+
+	//评估器，将源和目标的tag全部传递进来
+	FAggregatorEvaluateParameters EvaluateParameters;
+	EvaluateParameters.SourceTags = SourceTags;
+	EvaluateParameters.TargetTags = TargetTags;
+
+	float Armor = 0.f;
+	ExecutionParams.AttemptCalculateCapturedAttributeMagnitude(DamageStatics().ArmorDef,EvaluateParameters, Armor);
+	Armor = FMath::Max<float>(0.f, Armor);
+
+	const FGameplayModifierEvaluatedData EvaluatedData(DamageStatics().ArmorProperty,EGameplayModOp::Additive, Armor);
+	OutExecutionOutput.AddOutputModifier(EvaluatedData);
 }
