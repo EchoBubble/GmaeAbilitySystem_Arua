adeb669e4f4cfc92bba08a301a0d60b1af51d714
EchoBubble <1756865212@qq.com>
2025-06-16 18:11:31 +0800

    Custom Gameplay Effect Context

---
+++ Source/Arua_GAS/Public/AuraAbilityTypes.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include "GameplayEffectTypes.h"
+#include  "AuraAbilityTypes.generated.h"
+
+USTRUCT(BlueprintType)
+struct FAuraGameplayEffectContext : public FGameplayEffectContext
+{
+	GENERATED_BODY()
+	
+public:
+
+	bool IsCriticalHit() const {return bIsCriticalHit;}
+	bool IsBlockedHit() const {return bIsBlockedHit;}
+
+	void SetCriticalHit(bool bInCriticalHit) {bIsCriticalHit = bInCriticalHit;}
+	void SetBlockedHit(bool bInBlockedHit) {bIsBlockedHit = bInBlockedHit;}
+	
+	/** Returns the actual struct used for serialization, subclasses must override this! */
+	virtual UScriptStruct* GetScriptStruct() const
+	{
+		return FGameplayEffectContext::StaticStruct();
+	}
+
+	/** Custom serialization, subclasses must override this */
+	virtual bool NetSerialize(FArchive& Ar, class UPackageMap* Map, bool& bOutSuccess);
+	
+protected:
+
+	UPROPERTY()
+	bool bIsBlockedHit = false;
+
+	UPROPERTY()
+	bool bIsCriticalHit = false;
+	
+
+};
