3a17e6584ce4849141b9e2dd080728b9c32f1ff3
EchoBubble <162691516+EchoBubble@users.noreply.github.com>
2025-07-20 22:21:42 +0800

    Spawning the Rock Projectile

--- Source/Arua_GAS/Private/Actor/AuraProjectile.cpp
+++ Source/Arua_GAS/Private/Actor/AuraProjectile.cpp
@@ -6,6 +6,7 @@
 #include "AbilitySystemBlueprintLibrary.h"
 #include "AbilitySystemComponent.h"
 #include "NiagaraFunctionLibrary.h"
+#include "AbilitySystem/AuraAbilitySystemLibrary.h"
 #include "Arua_GAS/Arua_GAS.h"
 #include "Components/AudioComponent.h"
 #include "Components/SphereComponent.h"
@@ -62,7 +63,10 @@ void AAuraProjectile::OnSphereOverlap(UPrimitiveComponent* OverlappedComponent, 
 	{
 		return;
 	}
-
+	if (!UAuraAbilitySystemLibrary::IsNotFriend(DamageEffectSpecHandle.Data.Get()->GetContext().GetEffectCauser(),OtherActor))
+	{
+		return;
+	}
 	if (!bHit)
 	{
 		UGameplayStatics::PlaySoundAtLocation(this,ImpactSound,GetActorLocation(),FRotator::ZeroRotator);
