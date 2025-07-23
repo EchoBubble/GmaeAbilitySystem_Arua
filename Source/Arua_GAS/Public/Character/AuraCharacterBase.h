e224e454df2ce65a507f18a7c13eb93d3bb6b6cb
EchoBubble <1756865212@qq.com>
2025-07-23 09:46:03 +0800

    Impact Effects

--- Source/Arua_GAS/Public/Character/AuraCharacterBase.h
+++ Source/Arua_GAS/Public/Character/AuraCharacterBase.h
@@ -8,6 +8,7 @@
 #include "Interaction/CombatInterface.h"
 #include "AuraCharacterBase.generated.h"
 
+class UNiagaraSystem;
 class UGameplayAbility;
 class UGameplayEffect;
 class UAbilitySystemComponent;
@@ -32,6 +33,7 @@ public:
 	virtual bool IsDead_Implementation() const override;
 	virtual AActor* GetAvatar_Implementation() override;
 	virtual TArray<FTaggedMontage> GetAttackMontages_Implementation() override;
+	virtual UNiagaraSystem* GetBloodEffect_Implementation() override;
 	/* End Combat Interface*/
 	
 	UFUNCTION(NetMulticast, reliable)
@@ -89,6 +91,9 @@ protected:
 
 	UPROPERTY(EditAnywhere,BlueprintReadOnly)
 	TObjectPtr<UMaterialInstance> WeaponDissolveMaterialInstance;
+	
+	UPROPERTY(EditAnywhere,BlueprintReadOnly)
+	UNiagaraSystem* BloodEffect;
 private:
 
 	UPROPERTY(EditAnywhere,Category = "Abilities")
