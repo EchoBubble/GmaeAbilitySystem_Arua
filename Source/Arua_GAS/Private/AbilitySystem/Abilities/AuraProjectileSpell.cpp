2d26d435b9793e7d3abe4d899938fbd987f67045
EchoBubble <1756865212@qq.com>
2025-08-14 16:19:20 +0800

    Decrementing Minion Count

--- Source/Arua_GAS/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
+++ Source/Arua_GAS/Private/AbilitySystem/Abilities/AuraProjectileSpell.cpp
@@ -22,7 +22,7 @@ void UAuraProjectileSpell::ActivateAbility(const FGameplayAbilitySpecHandle Hand
 	
 }
 
-void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag)
+void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocation, const FGameplayTag& SocketTag, bool bOverridePitch, float PitchOverride)
 {
 	//const bool bIsServer = HasAuthority(&ActivationInfo);
 	const bool bIsServer = GetAvatarActorFromActorInfo()->HasAuthority();
@@ -32,6 +32,7 @@ void UAuraProjectileSpell::SpawnProjectile(const FVector& ProjectileTargetLocati
 		GetAvatarActorFromActorInfo(),SocketTag);//减少耦合，用接口获得位置
 
 	FRotator Rotation = (ProjectileTargetLocation - SocketLocation).Rotation();
+	if (bOverridePitch) Rotation.Pitch = PitchOverride;
 		
 	FTransform SpawnTransform;
 	SpawnTransform.SetLocation(SocketLocation);
